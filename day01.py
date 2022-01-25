2ec45e8b75c0b03d7ad7790462153c79b6c4c703
mercedes6115 <89904399+mercedes6115@users.noreply.github.com>
2022-01-25 16:37:31 +0900

    Add files via upload

---
+++ day01.py
@@ -0,0 +1,19 @@
+
+#print 출력함수에 대한 여러 유형
+
+'''month=input('할부 개월 입력: ')
+month = int(month)
+fee = int(50000/month)
+print(month,'개월 동안 내는 금액은', fee,'입니다.')
+print(f'{month}개월 동안 내는 금액은 {fee}입니다.')
+print('{}개월 동안 내는 금액은 {}입니다.'.format(month,fee))
+print('%d개월 동안 내는 금액은 %d입니다.' %(month,fee))
+'''
+
+'''
+a=float(input('실수1: '))
+b=float(input('실수2: '))
+c=round(a+b,4)
+
+print(a,'과',b,'의 합계는',c,'입니다')
+'''
